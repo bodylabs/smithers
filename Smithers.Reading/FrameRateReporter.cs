3f04a1817f000ac4053b6084931f5d8e699fb1bf
chenyang liu <chenyang.liu@bodylabs.com>
2014-10-16 15:31:42 -0400

    Simple Profiler to profile code executing time and support callback for processing

---
+++ Smithers.Reading/FrameRateReporter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Smithers.Reading.FrameData;
+
+namespace Smithers.Reading
+{
+    public class FpsChangedEventArgs : EventArgs
+    {
+        public double Fps { get; set; }
+
+        /// <summary>
+        /// TimeStamp when fps changed, in ms.
+        /// </summary>
+        public double TimeStamp { get; set; }
+
+        public FpsChangedEventArgs(double fps, double ts)
+        {
+            this.Fps = fps;
+            this.TimeStamp = ts;
+        }
+    }
+
+    public class FrameRateReporter : FrameReaderCallbacks
+    {
+        public double? StartTimeInMilliseconds { get; set; }
+        public double InstantFrameRate { get; set; }
+
+        public event EventHandler<FpsChangedEventArgs> FpsChanged;
+
+        public FrameRateReporter()
+        {
+        }
+
+        private double GetFps(double milliseconds)
+        {
+            return 1000.0 / milliseconds;
+        }
+
+        public double GetInstantFps(double currentTimeInMilliseconds)
+        {
+            if (this.StartTimeInMilliseconds.HasValue)
+            {
+                double fps = GetFps(currentTimeInMilliseconds - this.StartTimeInMilliseconds.Value);
+                this.StartTimeInMilliseconds = currentTimeInMilliseconds;
+                this.InstantFrameRate = fps;
+                return fps;
+            }
+
+            return 0.0;
+        }
+
+        public void FrameArrived(LiveFrame frame)
+        {
+
+            if (FpsChanged != null)
+            {
+                TimeSpan colorFrameRelativeTime = frame.NativeColorFrame.RelativeTime;
+
+                if (!this.StartTimeInMilliseconds.HasValue)
+                {
+                    this.StartTimeInMilliseconds = colorFrameRelativeTime.TotalMilliseconds;
+                }
+
+                FpsChanged(new object(), new FpsChangedEventArgs(this.GetInstantFps(frame.NativeColorFrame.RelativeTime.TotalMilliseconds), colorFrameRelativeTime.TotalMilliseconds));
+                
+            }
+        }
+    }
+}
---
+++ Smithers.Reading/SimpleProfiler.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Smithers.Reading
+{
+
+    /// <summary>
+    /// Encapsulate Profiling Result
+    /// </summary>
+    public class ProfilingResult
+    {
+        public decimal? ElapsedMilliseconds { get; set; } 
+    }
+
+    /// <summary>
+    /// Simple Profiler with support for getting frame rate
+    /// Callback function should accept ProfilingResult as parameter
+    /// 
+    /// Usage: 
+    ///     using(SimpleProfilerFactory.Profile("xxx", result => {
+    ///           Console.WriteLine(result.Label + " runs in [" + result.ElapsedMilliseconds + " ms]");})
+    ///     { 
+    ///             ...
+    ///             code snippet
+    ///             ...
+    ///     }
+    /// </summary>
+    public static class SimpleProfilerFactory
+    {
+        /// <summary>
+        /// Return a SimpleProfiler object which collect profiling infomation during its lifecycle
+        /// and pass the result to a custom prcoessing callback function 
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public static IDisposable Profile(Action<ProfilingResult> callback)
+        {
+            return new SimpleProfiler(callback);
+        }
+
+        public static IDisposable ProfileToConsole(string label)
+        {
+            return new SimpleProfiler(result => {
+                Console.WriteLine(label + " runs in [" + result.ElapsedMilliseconds + " ms]");
+            });
+        }
+    }
+
+    /// <summary>
+    /// Disposable SimpleProfiler to profile a segment of code
+    /// </summary>
+    public class SimpleProfiler : IDisposable
+    {
+        readonly Stopwatch _stopWatch = new Stopwatch();
+        private Action<ProfilingResult> _callback;
+
+        public SimpleProfiler(Action<ProfilingResult> callback)
+        {
+            _callback = callback;
+
+            _stopWatch.Start();
+        }
+
+        /// <summary>
+        /// Dispose the SimpleProfiler and invoke callback
+        /// </summary>
+        void IDisposable.Dispose()
+        {
+            _stopWatch.Stop();
+            if (_callback != null)
+            {
+                _callback(new ProfilingResult()
+                {
+                    ElapsedMilliseconds = _stopWatch.ElapsedMilliseconds
+                });
+            }
+        }
+    }
+}
